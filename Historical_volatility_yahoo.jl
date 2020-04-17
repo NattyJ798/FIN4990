4da9d0dbbb26b1c5529870f5362a4c86d131ada6
Nat Judge <50224920+NattyJ798@users.noreply.github.com>
2020-04-17 10:38:07 -0400

    Update Historical_volatility_yahoo.jl

--- Historical_volatility_yahoo.jl
+++ Historical_volatility_yahoo.jl
@@ -1,7 +1,9 @@
 # @ author Nathanael Judge
 # nzjudge@mtu.edu
 
+module Historical_Volatility
 using PyCall, DataFrames, CSV, Statistics
+export readData
 
 py"""
 import numpy as np
@@ -9,15 +11,20 @@ import pandas as pd
 import pandas_datareader.data as web
 import datetime
 
-def main():
-    ticker='GM'
-    end_date = datetime.date.today()
-    start_date=end_date.replace(year=end_date.year-1)
+def read_data(ticker, end_date, start_date):
+
 
     historical_prices = web.DataReader(ticker, data_source='yahoo',start=start_date,end=end_date)
-    print(historical_prices)
     historical_prices['Logreturn'] = np.log(historical_prices['Close'] / historical_prices['Close'].shift(1))
-    print(historical_prices['Logreturn'])
     historical_prices['Volatility'] = historical_prices['Logreturn'].rolling(window=30).std() * np.sqrt(252)
-    print(historical_prices['Volatility']);
+    return(historical_prices['Volatility'])
+
 """
+
+# readData
+# return the historical data
+function readData(ticker, end_date, start_date)
+return(py"read_data"(ticker,end_date, start_date))
+
+end
+end
